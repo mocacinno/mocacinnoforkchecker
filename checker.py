4f6ebcfb633c80feb27ef62500ed4651db0f74c8
root <root@node.ip-91-121-84.eu>
2018-04-24 07:46:06 +0000

    added a basic framework to add addresses

--- checker.py
+++ checker.py
@@ -1,1 +1,23 @@
-#the script
+import sys
+import argparse
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--address", help="query a single address")
+	parser.add_argument("--addressfile", help="query all addresses in this file")
+	parser.add_argument("--fork", help="query a single fork")
+	parser.add_argument("--showforks", help="show all forks")
+	args = parser.parse_args()
+	addresslist = []
+	forklist = []
+	if args.address:
+		addresslist.append(args.address)
+	if args.addressfile:
+		with open(args.addressfile) as file:
+			for address in file:
+				addresslist.append(address)
+	if len(addresslist) == 0:
+		sys.exit("no addresses available")
+	
+
+if __name__ == '__main__':
+	main()
