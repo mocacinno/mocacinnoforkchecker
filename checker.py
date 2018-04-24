8348d838f3d13de4de7fe437443428c1e3244d24
root <root@node.ip-91-121-84.eu>
2018-04-24 13:51:43 +0000

    added bicc

--- checker.py
+++ checker.py
@@ -8,26 +8,29 @@ import json
 
 def main():
 	available_forks = {
-		"B2X": get_b2x, 			#Bitcoin Segwit2X			#working
-		"BCA": get_bca, 			#Bitcoin Atom				#manual	
-		"BCD": get_bcd, 			#Bitcoin Diamond			#manual	
-		"BCH": get_bch, 			#Bitcoin Cash				#working
-		"BCHC": get_bchc, 			#Bitcoin Clashic			#working
-		"BCI": get_bci, 			#Bitcoin Intrest			#working
-		"BCX" : get_bcx, 			#BitcoinX					#working
-		"BPA" : get_bpa, 			#Bitcoin Pizza				#working
-		"BTC" : get_btc,			#Bitcoin					#working
-		"BTCP" : get_btcp,			#Bitcoin platinum			#no explorer
-		"BTF": get_btf, 			#Bitcoin Faith				#no explorer
-		"BTG": get_btg, 			#Bitcoin Gold				#working
-		"BTH": get_bth,				#Bitcoin Hot				#no explorer
-		"BTP": get_btp,				#Bitcoin Pay				#no explorer
-		"BTSQ": get_btsq,			#Bitcoin Community			#no explorer
-		"BTW": get_btw, 			#Bitcoin World				#no explorer
-		"BTX": get_btx, 			#Bitcore					#working
-		"CDY": get_cdy, 			#Bitcoin Candy (for of BCH)	#working
-		"LBTC": get_lbtc,			#Lightning Bitcoin			#manual
-		"SUPERBTC": get_superbtc, 	#Super Bitcoin				#manual
+		"B2X": get_b2x, 				#Bitcoin Segwit2X			#working
+		"BCA": get_bca, 				#Bitcoin Atom				#manual	
+		"BCD": get_bcd, 				#Bitcoin Diamond			#manual	
+		"BCH": get_bch, 				#Bitcoin Cash				#working
+		"BCHC": get_bchc, 				#Bitcoin Clashic			#working
+		"BCI": get_bci, 				#Bitcoin Intrest			#working
+		"BCX" : get_bcx, 				#BitcoinX					#working
+		"BICC" : get_bicc,				#BitClassic					#working
+		"BITCOINMINOR" : get_bitcoinminor,	#Bitcoin Minor			#no explorer
+		"BPA" : get_bpa, 				#Bitcoin Pizza				#working
+		"BTC" : get_btc,				#Bitcoin					#working
+		"BTCP" : get_btcp,				#Bitcoin platinum			#no explorer
+		"BTF": get_btf, 				#Bitcoin Faith				#no explorer
+		"BTG": get_btg, 				#Bitcoin Gold				#working
+		"BTH": get_bth,					#Bitcoin Hot				#no explorer
+		"BTP": get_btp,					#Bitcoin Pay				#no explorer
+		"BTSQ": get_btsq,				#Bitcoin Community			#no explorer
+		"BTW": get_btw, 				#Bitcoin World				#no explorer
+		"BTX": get_btx, 				#Bitcore					#working
+		"CDY": get_cdy, 				#Bitcoin Candy (for of BCH)	#working
+		"LBTC": get_lbtc,				#Lightning Bitcoin			#manual
+		"OBTC": get_obtc,				#Oil Bitcoin				#no explorer
+		"SUPERBTC": get_superbtc, 		#Super Bitcoin				#working
 	}
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--address", help="query a single address")
@@ -90,9 +93,23 @@ def main():
 		print
 		print "successlist"
 		print "***********"
-	for success in successes:
-		print success
+		for success in successes:
+			print success
+		print
+		print "-------------------------------------------------------------------------------------------"
+		print "| once again, if you import your private key into ANY unknown/untrusted wallet,           |"
+		print "| you risk losing your unspent outputs on all other chains!!!                             |"
+		print "| proceed with caution                                                                    |"
+		print "|*****************************************************************************************|"
+		print "| at least make sure your wallets on the most important chains are empty before importing |"
+		print "| their private keys into unknown wallets!!!                                              |"
+		print "-------------------------------------------------------------------------------------------"
+		print
+		print "if you like this project, consider some of the \"free\" coins you got from these forks to me ;)"
+		print "BTC/BCH/BTX/B2X/...: 1MocACiWLM8bYn8pCrYjy6uHq4U3CkxLaa"
+		print "BTG: GeeXaL3TKCjtdFS78oCrPsFBkEFt9fxuZF"
 
+###############################################################################################	
 
 def veranderprefix(address, prefix):
 	try:
@@ -163,12 +180,16 @@ def fromiquidus(address, baseurl, chain):
 			val = float(r.text)
 			return val
 		except ValueError:
-			print stderror
+			if json.loads(r.text)['error'] == "address not found.":
+				return 0
+			print stderror + " 1"
 			return 0
 	except:
-		print stderror
+		print stderror + " 2"
 		return 0
-###############################################################################################		
+		
+###############################################################################################
+
 def get_bchc(address):
 	chain = "BCHC"
 	print "\t checking address " + address + " on the " + chain + " chain"	
@@ -189,14 +210,16 @@ def get_btx(address):
 	chain = "BCX"
 	print "\t checking address " + address + " on the " + chain + " chain"	
 	return fromchainz(address, 'https://chainz.cryptoid.info/btx/', chain)	
-		
-		
+			
 def get_bca(address):
 	print "\tno explorer with an api found, check manually on https://bitcoinatom.net/ (prefix 23)"
 		
 def get_btsq(address):
 	print "\tdidn't find a single explorer for bitcoin community (btsq) (prefix 63)"
-
+	
+def get_obtc(address):
+	print "\tdidn't find a single explorer for oil bitcoin (obtc)"
+	
 def get_cdy(address):
 	chain = "CDY"
 	address = veranderprefix(address, 0x1c)
@@ -205,6 +228,9 @@ def get_cdy(address):
 		
 def get_bth(address):
 	print "\tdidn't find a single explorer for bitcoin hot (bth) (prefix 40)"
+	
+def get_bitcoinminor(address):
+	print "\tdidn't find a single explorer for bitcoin minor (bitcoinminor)"
 
 def get_btp(address):
 	print "\tdidn't find a single explorer for bitcoin pay (btp) (prefix 0x38)"
@@ -214,6 +240,11 @@ def get_bpa(address):
 	address = veranderprefix(address, 55)
 	print "\t checking address " + address + " on the " + chain + " chain"	
 	return fromiquidus(address, 'http://47.100.55.227/ext/', chain)
+
+def get_bicc(address):
+	chain = "BICC"
+	print "\t checking address " + address + " on the " + chain + " chain"	
+	return fromiquidus(address, 'http://18.216.251.169/ext/', chain)
 
 def get_btw(address):
 	print "\tdidn't find a single explorer for bitcoin world (btw) (prefix 73)"
@@ -236,6 +267,9 @@ def get_bch(address):
 	return frominsightapi(address, 'https://bitcoincash.blockexplorer.com/api/', chain)	
 				
 def get_superbtc(address):
+	chain = "SUPERBTC"
+	print "\t checking address " + address + " on the " + chain + " chain"	
+	return frominsightapi(address, 'http://block.superbtc.org/insight-api/', chain)	
 	print "\tSUPERBTC api down, check manually at block.superbtc.org"
 	
 def get_b2x(address):
@@ -249,13 +283,13 @@ def get_lbtc(address):
 def get_bcd(address):
 	print "\tLBTC api down, check manually at explorer.btcd.io"
 	
-
 def get_btg(address):
 	chain = "BTG"
 	address = veranderprefix(address, 38)
 	print "\t checking address " + address + " on the " + chain + " chain"	
 	return frominsightapi(address, 'https://btgexplorer.com/api/', chain)	
 
+###############################################################################################	
 
 if __name__ == '__main__':
 	main()
